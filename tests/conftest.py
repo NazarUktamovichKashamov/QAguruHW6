fec540f7a98380ceae14ae9034ccb21d762aa360
kashamovnu <nazar.kashamov@gmail.com>
2024-07-12 16:05:41 +0400

    Частично выполнил задание.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,19 @@
-import pytest
-
 import os.path
 import shutil
 from zipfile import ZipFile
 
+import pytest
+
 from set_path import ZIP_DIR, RESOURCES_DIR, ZIP_FILE
 
+
 @pytest.fixture(scope='session', autouse=True)
-def test_pack_files_to_zipchick():
+def test_create_zip_archive():
     if not os.path.exists(ZIP_DIR):
         os.mkdir(ZIP_DIR)
-    with ZipFile(ZIP_FILE, 'w') as zipfile:
+    with ZipFile(ZIP_FILE, 'w') as zip_file:
         for file in os.listdir(RESOURCES_DIR):
-            ZipFile.write(os.path.join(RESOURCES_DIR, file), file)
-
+            zip_file.write(os.path.join(RESOURCES_DIR, file), file)
 
     yield
 
--- tests/test_files.py
+++ tests/test_files.py
@@ -14,5 +14,27 @@ def test_check_pdf_in_archive():
         with zip_file.open(file_name) as file:
             reader = PdfReader(file)
 
-            assert len(reader.pages) == 1011
-            assert "PDF Form Example”" in reader.pages[1].extract_text()
+            assert len(reader.pages) == 1
+            assert "PDF Form Example" in reader.pages[0].extract_text()
+
+
+def test_check_xlsx_in_archive():
+    file_name = 'sample1.xlsx'
+    with ZipFile(ZIP_FILE) as zip_file:
+        with zip_file.open(file_name) as file:
+            workbook = load_workbook(file)
+            title_list = workbook.sheetnames
+            sheet = workbook.active
+
+            assert sheet.cell(row=2, column=2).value == 456
+
+
+def test_check_csv_in_archive_csv():
+    file_name = 'sample3.csv'
+    with ZipFile(ZIP_FILE) as zip_file:
+        with zip_file.open(file_name) as file:
+            csv_file = file.read().decode(encoding='utf-8')
+            content = list(csv.reader(csv_file.splitlines(), delimiter=';'))
+
+            assert content[0][0] == 'Game Number, "Game Length"'
+
